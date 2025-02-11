e0634ee352ef4b25a680cd02435cf722baca092a
Jhonathan562 <guevarajhonathan123@gmail.com>
2025-02-10 21:56:35 -0500

    lab2

---
+++ laboratorio_2_inciso_A_B.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Inciso o punto 8 (a)
+# Datos de Jhonathan
+# Se define h[n] y x[n] 
+h = np.array([5,6,0,0,7,4,7]) # codigo estudiantil Jhonathan
+x = np.array([1,0,1,3,1,0,5,4,6,1]) # cedula ciudadana Jhonathan
+
+# Graficar h[n] Jhonathan
+t = np.arange(len(h)) 
+plt.figure(figsize=(8, 4))
+plt.stem(t, h)
+plt.xlabel('n')
+plt.ylabel('h[n]')
+plt.title('h[n] Jhonathan')
+plt.grid()
+plt.show()
+
+# Graficar x[n] Jhonathan
+t = np.arange(len(x))
+plt.figure(figsize=(8, 4))
+plt.stem(t, x)
+plt.xlabel('n')
+plt.ylabel('x[n]')
+plt.title('x[n] Jhonathan')
+plt.grid()
+plt.show()
+
+y = np.convolve(x, h, mode='full') #Calcular la convolucion entre h[n] y x[n]
+print("Señal de la convolucion entre h[n] y x[n] osea y[n] de Jhonathan:", y) #Imprimir el resultado de la convolucion
+
+# Graficar la convolución o y[n] Jhonathan
+t = np.arange(len(y))
+plt.figure(figsize=(8, 4))
+plt.stem(t, y)
+plt.xlabel('n')
+plt.ylabel('y[n]')
+plt.title('Convolución de h[n] y x[n] Jhonathan')
+plt.grid()
+plt.show()
+
+
+# Datos de Jose
+# Se define h[n] y x[n] 
+h = np.array([5,6,0,0,6,8,3]) # codigo estudiantil Jose
+x = np.array([1,0,2,7,1,5,0,7,2,5]) # cedula ciudadana Jose
+
+# Graficar h[n] Jose
+t = np.arange(len(h)) 
+plt.figure(figsize=(8, 4))
+plt.stem(t, h)
+plt.xlabel('n')
+plt.ylabel('h[n]')
+plt.title('h[n] Jose')
+plt.grid()
+plt.show()
+
+# Graficar x[n] Jose
+t = np.arange(len(x))
+plt.figure(figsize=(8, 4))
+plt.stem(t, x)
+plt.xlabel('n')
+plt.ylabel('x[n]')
+plt.title('x[n] Jose')
+plt.grid()
+plt.show()
+
+y = np.convolve(x, h, mode='full') #Calcular la convolucion entre h[n] y x[n]
+print("Señal de la convolucion entre h[n] y x[n] osea y[n] de Jose:", y) #Imprimir el resultado de la convolucion
+
+# Graficar la convolución o y[n] Jose
+t = np.arange(len(y))
+plt.figure(figsize=(8, 4))
+plt.stem(t, y)
+plt.xlabel('n')
+plt.ylabel('y[n]')
+plt.title('Convolución de h[n] y x[n] Jose')
+plt.grid()
+plt.show()
+
+
+
+# Inciso o pun to 8(b)
+Ts = 1.25e-3  #𝑇𝑠 = 1.25𝑚s
+n = np.arange(9) # 𝑝𝑎𝑟𝑎 0 ≤ 𝑛 < 9 
+f = 100  # 100𝑛𝑇s
+x1 = np.cos(2 * np.pi * f * n * Ts) #𝑥1[𝑛𝑇𝑠] = cos(2𝜋100𝑛𝑇𝑠)
+x2 = np.sin(2 * np.pi * f * n * Ts) #𝑥2[𝑛𝑇𝑠] = sin(2𝜋100𝑛𝑇𝑠)
+correlacion = np.correlate(x1, x2, mode='full') # Correlacione entre ambas señales
+print("Correlación cruzada osea el vector o resultado", correlacion)
+
+# Graficar Correlacion entre ambas señales
+t_corr = np.arange(-len(n) + 1, len(n))
+plt.figure(figsize=(8, 4))
+plt.stem(t_corr, correlacion)
+plt.xlabel('Desplazamiento')
+plt.ylabel('Correlación')
+plt.title('Correlación cruzada entre x1[n] y x2[n]')
+plt.grid()
+plt.show()
+
+
+
---
+++ laboratorio_2_inciso_C.py
@@ -0,0 +1,151 @@
+import wfdb
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+EMG = "Datos_EMG/emg_neuropathy"
+
+lecturasignal = wfdb.rdrecord(EMG)
+signal = lecturasignal.p_signal[:,0]  
+fs = lecturasignal.fs  
+numero_datos = len(signal) 
+muestreo=int(5*fs)
+time = [i / fs for i in range(numero_datos)]  
+signal = signal[:muestreo]
+time = time[:muestreo]
+
+plt.figure(figsize=(12,4))
+plt.plot(time, signal, color="red")
+plt.xlabel("Tiempo (s)")
+plt.ylabel("Amplitud (mv)")
+plt.title("Señal EMG neuropatia")
+plt.legend()
+plt.grid()
+plt.show()
+
+suma=0
+for i in range(len(signal)):
+    suma += signal[i]
+media = suma/ len(signal)
+print(f"Media de la señal: {media:.4f}")
+
+longitud_vector = 0
+for _ in signal:
+    longitud_vector +=1
+print(f"Longitud del vector: {longitud_vector}")
+
+desviacion = 0
+for i in range(len(signal)):
+    desviacion += (signal[i] - media) ** 2
+desviacion_estandar = (desviacion/len(signal)) ** 0.5
+print(f"Desviación estándar: {desviacion_estandar:.4f}")
+
+coeficiente_de_variacion = desviacion_estandar/ media if media != 0 else float ('nan')
+print(f"Coeficiente de variación: {coeficiente_de_variacion:.4f}")
+
+def calcular_mediana(datos):
+    datos_ordenados = sorted(datos)
+    n = len(datos_ordenados)
+    if n % 2 == 1:
+        mediana = datos_ordenados[n // 2]  
+    else:
+        mediana = (datos_ordenados[n // 2 - 1] + datos_ordenados[n // 2]) / 2  # Promedio de los dos centrales
+    return mediana
+
+mediana = calcular_mediana(signal)
+print(f"La mediana es : {mediana:.4f}")
+
+mediana_librerias = np.median(signal)
+media_librerias = np.mean(signal)
+longitud_vector_librerias = len(signal)
+desviacion_librerias = np.std(signal)
+coeficiente_variacion_librerias = desviacion_librerias / media_librerias if media_librerias != 0 else np.nan
+
+print(f"Media de la señal con librerias: {media_librerias:.4f}")
+print(f"Longitud del vector con librerias: {longitud_vector_librerias}")
+print(f"Desviación estándar con librerias: {desviacion_librerias:.4f}")
+print(f"Coeficiente de variación con librerias: {coeficiente_variacion_librerias:.4f}")
+print(f"La mediana es : {mediana_librerias:.4f}")
+
+
+t = np.linspace(0, 1, fs, endpoint=False) 
+N = len(t)
+
+frequencies = np.fft.fftfreq(N, 1/fs)
+spectrum = np.fft.fft(signal) / N
+magnitude = 2 * np.abs(spectrum[:N//2]) 
+
+
+plt.figure(figsize=(12,4))
+plt.plot(frequencies[:N//2], magnitude, 'orange')
+plt.xlabel('Frecuencia (Hz)')
+plt.ylabel('Magnitud')
+plt.title('Espectro de la señal normalizado')
+plt.grid()
+plt.show()
+
+psd = (magnitude ** 2) / N
+
+plt.figure(figsize=(12,4))
+plt.plot(frequencies[:N//2], psd, 'violet')
+plt.xlabel('Frecuencia (Hz)')
+plt.ylabel('Densidad Espectral')
+plt.title('Espectro de la señal de Densidad Espectral')
+plt.grid()
+plt.show()
+
+
+
+suma=0
+for i in range(len(magnitude)):
+    suma += magnitude[i]
+media = suma/ len(magnitude)
+print(f"Media de la señal en cuanto a la frecuencia: {media:.4f}")
+
+desviacion = 0
+for i in range(len(magnitude)):
+    desviacion += (magnitude[i] - media) ** 2
+desviacion_estandar = (desviacion/len(signal)) ** 0.5
+print(f"Desviación estándar en cuanto a la frecuencia: {desviacion_estandar:.4f}")
+
+mediana = calcular_mediana(magnitude)
+print(f"La mediana en cuanto a la frecuencia: {mediana:.4f}")
+
+
+mediana_librerias = np.median(magnitude)
+media_librerias = np.mean(magnitude)
+desviacion_librerias = np.std(magnitude)
+
+print(f"Media de la señal con librerias en cuanto a la frecuencia: {media_librerias:.4f}")
+print(f"Desviación estándar con librerias en cuanto a la frecuencia: {desviacion_librerias:.4f}")
+print(f"La mediana en cuanto a la frecuencia : {mediana_librerias:.4f}")
+
+plt.figure(figsize=(8, 4))
+plt.hist(magnitude, bins=50, color='orange', alpha=0.7, edgecolor='black', density=True)
+plt.xlabel("Magnitud")
+plt.ylabel("Frecuencia (Hz)")
+plt.title("Histograma de la Frecuencia (5s)")
+plt.grid()
+plt.show()
+
+
+plt.figure(figsize=(12, 8))
+plt.subplot(4, 1, 1)
+plt.plot(signal, label="Signal Biomedical EMG neuropathy", color="red")
+plt.title("Signal Biomedical EMG neuropath")
+plt.legend()
+
+plt.subplot(4, 1, 2)
+plt.plot(magnitude[:muestreo//2], label="Signal at transformation a Fourier", color="orange")
+plt.title("Signal at transformation a Fourier")
+plt.legend()
+
+plt.subplot(4, 1, 3)
+plt.plot(psd[:muestreo//2], label="Signal Density Espectral a Potencial", color="violet")
+plt.title("Signal Density Espectral a Potencial")
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
